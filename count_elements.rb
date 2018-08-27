910ed65943cea7f826bebc8367e263ba88f7e7fd
Jacob Berman <jacobberman1995@gmail.com>
2018-08-27 21:25:37 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  unique = array.uniq
+  hash = {}
+  array.each do |element|
+    if hash.has_key?(element)
+      hash[element]+= 1
+    else
+      hash[element] = 1
+    end
+  end
+hash
 end
- 
